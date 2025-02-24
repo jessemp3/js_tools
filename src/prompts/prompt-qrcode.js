a2b6e699632e0be3effbe6d3b02809d8b51d15af
jesse <nitchsx@gmail.com>
2025-02-24 00:28:39 -0300

    v1.0.7

--- src/prompts/prompt-qrcode.js
+++ src/prompts/prompt-qrcode.js
@@ -4,14 +4,16 @@ const qrcodePrompt = [
    {
     name: "link", 
     description: chalk.bgWhite("Digite o link para gerar o QR Code"),
+    required: true, // Adicionando required
+    message: chalk.red.italic("Link é obrigatório") // Mensagem de erro
    },
    {
     name: "type",
-    description: chalk.bgWhite("Escolha o tipo de QR Code(1 - NORMAL OU 2 - TERMINAL"),
-    pattern: /^[1-2]*$/,
+    description: chalk.bgWhite("Escolha o tipo de QR Code(1 - NORMAL OU 2 - TERMINAL)"),
+    pattern: /^[1-2]$/,
     message: chalk.red.italic("Escolha 1 ou 2"),
     required: true
    }
-]
+];
 
 export default qrcodePrompt;
